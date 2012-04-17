09ba4f5d43c801abaed5ce381c8aa17ea2d32206
Tyler Liden <tylerliden@gmail.com>
2012-04-17 11:22:41 -0400

    removed

--- AviLoaderTester.java
+++
@@ -1,7 +0,0 @@
-public class AviLoaderTester {
-    public static void main(String[] args) {	   
-        aviLoader gs = new aviLoader(150, "33.avi");
-        Thread th = new Thread(gs); // thread to speed things up
-        th.start();
-    }
-}
