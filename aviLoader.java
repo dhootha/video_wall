056a6730546d98e8e344d42c8e503ef0453ee00f
Tyler Liden <tylerliden@gmail.com>
2012-04-21 14:01:45 -0400

    cleaned up testing code

--- aviLoader.java
+++ aviLoader.java
@@ -1,27 +1,21 @@
-//import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
 import java.awt.image.BufferedImage;
-
 import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.OpenCVFrameGrabber;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
-//import com.googlecode.javacv.CanvasFrame; //used to get canvas
 
 public class aviLoader{
 	BufferedImage outputImage;
 	int frameNum;
-    IplImage image;
     String fileName;
     boolean looking = true;
-    //CanvasFrame canvas = new CanvasFrame("Video"); //debug code to show frame
+    
     public aviLoader(int frame, String name) {
     	frameNum = frame;
     	fileName = name;
-        //canvas.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE); //debug code to show frame
-    }
-    
+    }   
     
     public BufferedImage getFrame() {
-        FrameGrabber grabber = new OpenCVFrameGrabber("vids\\"+ fileName); // for AVI
+        FrameGrabber grabber = new OpenCVFrameGrabber(fileName); // for AVI
         try {
         	int i = 0;
             grabber.start();
@@ -32,16 +26,14 @@ public class aviLoader{
                 	i++;
                 	if (i == frameNum)
                 	{
-                		img.copyTo(outputImage);
-                		//canvas.showImage(img); //debug code to show frame
-                		//cvSaveImage("FRAME.jpg", img); //saves in working directory
+                		outputImage = img.getBufferedImage();//saves frame to a buffered image
                 		grabber.stop();
                 		looking = false;
                 	}
                 }
             }
         } catch (Exception e) {
-        }
+        }      
 		return outputImage;
     }
 }
